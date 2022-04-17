b2dcf3b156b15ac6f22417b778a32dff31ad5dbe
bhavan <bhavanl@techprovint.com>
2022-04-17 19:30:35 +0530

    test data

--- src/components/pages/Testdata.jsx
+++ src/components/pages/Testdata.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { copy_to_clipboard } from "../../functions/util";
 import faker from "faker";
 
 function TestdataComponent({
@@ -59,27 +58,8 @@ function TestdataComponent({
 export default function Testdata() {
 	const [wordCount, setWordCount] = React.useState(100);
 
-	const [data, setData] = React.useState({
-		name: faker.name.findName(),
-		email: faker.internet.email(),
-		street: faker.address.streetName(),
-		city: faker.address.cityName(),
-		state: faker.address.state(),
-		company: faker.company.companyName(),
-		department: faker.commerce.department(),
-		product: faker.commerce.productName(),
-		phone: faker.phone.phoneNumber(),
-		vehicle: faker.vehicle.vehicle(),
-		password: faker.internet.password(),
-		uuid: faker.datatype.uuid(),
-		sentences: faker.lorem.sentences(),
-		paragraphs: faker.lorem.paragraphs(),
-		words: faker.random.alpha(wordCount),
-	});
-
-	const generateData = (e) => {
-		// e.preventDefault();
-		setData({
+	const getData = () => {
+		return {
 			name: faker.name.findName(),
 			email: faker.internet.email(),
 			street: faker.address.streetName(),
@@ -95,7 +75,16 @@ export default function Testdata() {
 			sentences: faker.lorem.sentences(),
 			paragraphs: faker.lorem.paragraphs(),
 			words: faker.random.alpha(wordCount),
-		});
+			timezone: faker.address.timeZone(),
+			country: faker.address.country(),
+		};
+	};
+
+	const [data, setData] = React.useState(getData);
+
+	const generateData = (e) => {
+		e.preventDefault();
+		setData(getData);
 		console.log(
 			faker.system.fileName(),
 			faker.system.fileType(),
@@ -112,58 +101,52 @@ export default function Testdata() {
 			faker.finance.bitcoinAddress(),
 			faker.finance.transactionDescription(),
 			faker.finance.creditCardNumber(),
-			faker.commerce.color(),
-			faker.commerce.productName(),
-			faker.address.country(),
-			faker.address.timeZone()
+			faker.commerce.color()
 		);
-	};
-
-	const handleChange = (e) => {
-		e.preventDefault();
-		copy_to_clipboard(e.target.id);
 	};
 
 	return (
 		<>
 			<div className="container mt-4">
 				<div className="row">
-					<div className="col">
-						<TestdataComponent name={"Name"} datatype={data.name} />
-						<TestdataComponent name={"Email"} datatype={data.email} />
-						<TestdataComponent name={"Street"} datatype={data.street} />
-						<TestdataComponent name={"City"} datatype={data.city} />
-						<TestdataComponent name={"State"} datatype={data.state} />
-						<TestdataComponent name={"Company"} datatype={data.company} />
-						<TestdataComponent name={"Department"} datatype={data.department} />
-						<TestdataComponent
-							isRandomWord={true}
-							name={"Random Words"}
-							datatype={data.words}
-							wordCount={wordCount}
-							handleWordCount={(e) => {
-								setWordCount(Number(e.target.value));
-							}}
-						/>
-					</div>
-					<div className="col">
-						<TestdataComponent name={"Product"} datatype={data.product} />
-						<TestdataComponent name={"Phone"} datatype={data.phone} />
-						<TestdataComponent name={"Vehicle"} datatype={data.vehicle} />
-						<TestdataComponent name={"Password"} datatype={data.password} />
-						<TestdataComponent name={"UUID"} datatype={data.uuid} />
-						<TestdataComponent name={"Sentences"} datatype={data.sentences} />
-						<TestdataComponent name={"Paragraphs"} datatype={data.paragraphs} />
-					</div>
-				</div>
-				<div className="row">
 					<input
 						type="button"
-						className="btn btn-success"
+						className="btn btn-outline-secondary"
 						id="gen-random-data-btn"
 						value="Generate Data"
 						onClick={generateData}
 					/>
+				</div>
+				<div className="row">
+					<TestdataComponent name={"Name"} datatype={data.name} />
+					<TestdataComponent name={"Email"} datatype={data.email} />
+					<TestdataComponent name={"Password"} datatype={data.password} />
+					<TestdataComponent name={"Timezone"} datatype={data.timezone} />
+					<TestdataComponent name={"Phone"} datatype={data.phone} />
+					<TestdataComponent name={"Street"} datatype={data.street} />
+					<TestdataComponent name={"City"} datatype={data.city} />
+					<TestdataComponent name={"Country"} datatype={data.country} />
+					<TestdataComponent name={"State"} datatype={data.state} />
+					<TestdataComponent name={"Company"} datatype={data.company} />
+					<TestdataComponent name={"Department"} datatype={data.department} />
+					<TestdataComponent name={"Product"} datatype={data.product} />
+					<TestdataComponent name={"Vehicle"} datatype={data.vehicle} />
+					<TestdataComponent name={"UUID"} datatype={data.uuid} />
+					<TestdataComponent name={"Sentences"} datatype={data.sentences} />
+					<TestdataComponent name={"Paragraphs"} datatype={data.paragraphs} />
+					<TestdataComponent
+						isRandomWord={true}
+						name={"Random Words"}
+						datatype={data.words}
+						wordCount={wordCount}
+						handleWordCount={(e) => {
+							setWordCount(Number(e.target.value));
+						}}
+					/>
+					{/* 
+						<div className="col"></div>
+						<div className="col"></div> 
+					*/}
 				</div>
 			</div>
 		</>
