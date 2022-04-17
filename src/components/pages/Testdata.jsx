7ef3d19a25e4f7db3e557957ba2a7125d28b8e66
bhavan <bhavanl@techprovint.com>
2022-04-17 19:16:43 +0530

    fix test data

--- src/components/pages/Testdata.jsx
+++ src/components/pages/Testdata.jsx
@@ -2,6 +2,60 @@ import React from "react";
 import { copy_to_clipboard } from "../../functions/util";
 import faker from "faker";
 
+function TestdataComponent({
+	isRandomWord = false,
+	wordCount,
+	handleWordCount,
+	datatype,
+	name,
+}) {
+	const handleChange = (e) => {
+		e.preventDefault();
+		navigator.clipboard.writeText(e.target.value);
+	};
+
+	return (
+		<>
+			{isRandomWord ? (
+				<div className="m-3 row">
+					<label className="col-sm-2 col-form-label">
+						{name}
+						<input
+							type="text"
+							className="form-control mt-1"
+							id="random-words-count"
+							value={wordCount}
+							onChange={handleWordCount}
+						/>
+					</label>
+					<div className="col-md">
+						<textarea
+							type="text"
+							className="form-control h-100"
+							id={datatype}
+							value={datatype}
+							onChange={handleChange}
+						></textarea>
+					</div>
+				</div>
+			) : (
+				<div className="m-3 row">
+					<label className="col-sm-2 col-form-label">{name}: </label>
+					<div className="col-md">
+						<input
+							type="text"
+							className="form-control"
+							id={datatype}
+							value={datatype}
+							onChange={handleChange}
+						/>
+					</div>
+				</div>
+			)}
+		</>
+	);
+}
+
 export default function Testdata() {
 	const [wordCount, setWordCount] = React.useState(100);
 
@@ -17,7 +71,7 @@ export default function Testdata() {
 		phone: faker.phone.phoneNumber(),
 		vehicle: faker.vehicle.vehicle(),
 		password: faker.internet.password(),
-		transactionId: faker.datatype.uuid(),
+		uuid: faker.datatype.uuid(),
 		sentences: faker.lorem.sentences(),
 		paragraphs: faker.lorem.paragraphs(),
 		words: faker.random.alpha(wordCount),
@@ -37,12 +91,32 @@ export default function Testdata() {
 			phone: faker.phone.phoneNumber(),
 			vehicle: faker.vehicle.vehicle(),
 			password: faker.internet.password(),
-			transactionId: faker.datatype.uuid(),
+			uuid: faker.datatype.uuid(),
 			sentences: faker.lorem.sentences(),
 			paragraphs: faker.lorem.paragraphs(),
 			words: faker.random.alpha(wordCount),
 		});
-		console.log(faker);
+		console.log(
+			faker.system.fileName(),
+			faker.system.fileType(),
+			faker.system.fileExt(),
+			faker.random.alphaNumeric(),
+			faker.name.jobTitle(),
+			faker.name.jobType(),
+			faker.name.jobDescriptor(),
+			faker.name.prefix(),
+			faker.name.title(),
+			faker.music.genre(),
+			faker.internet.userName(),
+			faker.internet.url(),
+			faker.finance.bitcoinAddress(),
+			faker.finance.transactionDescription(),
+			faker.finance.creditCardNumber(),
+			faker.commerce.color(),
+			faker.commerce.productName(),
+			faker.address.country(),
+			faker.address.timeZone()
+		);
 	};
 
 	const handleChange = (e) => {
@@ -55,215 +129,31 @@ export default function Testdata() {
 			<div className="container mt-4">
 				<div className="row">
 					<div className="col">
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Name: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-name"
-									value={data.name}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Email: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-email"
-									value={data.email}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Street: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-street"
-									value={data.street}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">City: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-city"
-									value={data.city}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">State: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-state"
-									value={data.state}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Company: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-company"
-									value={data.company}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">
-								Random Words:
-								<input
-									type="text"
-									className="form-control mt-1"
-									id="random-words-count"
-									value={wordCount}
-									onChange={(e) => {
-										setWordCount(Number(e.target.value));
-									}}
-								/>
-							</label>
-							<div className="col-md">
-								<textarea
-									type="text"
-									className="form-control h-100"
-									id="random-words"
-									value={data.words}
-									onChange={() => {}}
-									onClick={handleChange}
-								></textarea>
-							</div>
-						</div>
-
+						<TestdataComponent name={"Name"} datatype={data.name} />
+						<TestdataComponent name={"Email"} datatype={data.email} />
+						<TestdataComponent name={"Street"} datatype={data.street} />
+						<TestdataComponent name={"City"} datatype={data.city} />
+						<TestdataComponent name={"State"} datatype={data.state} />
+						<TestdataComponent name={"Company"} datatype={data.company} />
+						<TestdataComponent name={"Department"} datatype={data.department} />
+						<TestdataComponent
+							isRandomWord={true}
+							name={"Random Words"}
+							datatype={data.words}
+							wordCount={wordCount}
+							handleWordCount={(e) => {
+								setWordCount(Number(e.target.value));
+							}}
+						/>
 					</div>
 					<div className="col">
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Department: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-department"
-									value={data.department}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Product: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-product"
-									value={data.product}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Phone: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-phone"
-									value={data.phone}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Vehicle: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-vehicle"
-									value={data.vehicle}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Password: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-password"
-									value={data.password}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">UUID: </label>
-							<div className="col-md">
-								<input
-									type="text"
-									className="form-control"
-									id="random-transactionId"
-									value={data.transactionId}
-									onChange={() => {}}
-									onClick={handleChange}
-								/>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Sentences: </label>
-							<div className="col-md">
-								<textarea
-									type="text"
-									className="form-control h-100"
-									id="random-sentences"
-									value={data.sentences}
-									onChange={() => {}}
-									onClick={handleChange}
-								></textarea>
-							</div>
-						</div>
-						<div className="m-3 row">
-							<label className="col-sm-2 col-form-label">Paragraphs: </label>
-							<div className="col-md">
-								<textarea
-									type="text"
-									className="form-control h-100"
-									id="random-paragraphs"
-									value={data.paragraphs}
-									onChange={() => {}}
-									onClick={handleChange}
-								></textarea>
-							</div>
-						</div>
+						<TestdataComponent name={"Product"} datatype={data.product} />
+						<TestdataComponent name={"Phone"} datatype={data.phone} />
+						<TestdataComponent name={"Vehicle"} datatype={data.vehicle} />
+						<TestdataComponent name={"Password"} datatype={data.password} />
+						<TestdataComponent name={"UUID"} datatype={data.uuid} />
+						<TestdataComponent name={"Sentences"} datatype={data.sentences} />
+						<TestdataComponent name={"Paragraphs"} datatype={data.paragraphs} />
 					</div>
 				</div>
 				<div className="row">
