1d4bbcfc0eddda7cc05e349c2055d7ece1c5985c
bhavan <bhavanl@techprovint.com>
2022-04-17 19:45:19 +0530

    add aditional test data

--- src/components/pages/Testdata.jsx
+++ src/components/pages/Testdata.jsx
@@ -56,9 +56,7 @@ function TestdataComponent({
 }
 
 export default function Testdata() {
-	const [wordCount, setWordCount] = React.useState(100);
-
-	const getData = () => {
+	const getData = (count) => {
 		return {
 			name: faker.name.findName(),
 			email: faker.internet.email(),
@@ -74,35 +72,33 @@ export default function Testdata() {
 			uuid: faker.datatype.uuid(),
 			sentences: faker.lorem.sentences(),
 			paragraphs: faker.lorem.paragraphs(),
-			words: faker.random.alpha(wordCount),
+			words: faker.random.alpha(count),
 			timezone: faker.address.timeZone(),
 			country: faker.address.country(),
+			fileName: faker.system.fileName(),
+			fileType: faker.system.fileType(),
+			fileExt: faker.system.fileExt(),
+			jobTitle: faker.name.jobTitle(),
+			jobType: faker.name.jobType(),
+			jobDescriptor: faker.name.jobDescriptor(),
+			prefix: faker.name.prefix(),
+			title: faker.name.title(),
+			genre: faker.music.genre(),
+			userName: faker.internet.userName(),
+			url: faker.internet.url(),
+			bitcoinAddress: faker.finance.bitcoinAddress(),
+			transactionDescription: faker.finance.transactionDescription(),
+			creditCardNumber: faker.finance.creditCardNumber(),
+			color: faker.commerce.color(),
 		};
 	};
 
-	const [data, setData] = React.useState(getData);
+	const [wordCount, setWordCount] = React.useState(100);
+	const [data, setData] = React.useState(getData(wordCount));
 
 	const generateData = (e) => {
 		e.preventDefault();
-		setData(getData);
-		console.log(
-			faker.system.fileName(),
-			faker.system.fileType(),
-			faker.system.fileExt(),
-			faker.random.alphaNumeric(),
-			faker.name.jobTitle(),
-			faker.name.jobType(),
-			faker.name.jobDescriptor(),
-			faker.name.prefix(),
-			faker.name.title(),
-			faker.music.genre(),
-			faker.internet.userName(),
-			faker.internet.url(),
-			faker.finance.bitcoinAddress(),
-			faker.finance.transactionDescription(),
-			faker.finance.creditCardNumber(),
-			faker.commerce.color()
-		);
+		setData(getData(wordCount));
 	};
 
 	return (
@@ -118,35 +114,62 @@ export default function Testdata() {
 					/>
 				</div>
 				<div className="row">
-					<TestdataComponent name={"Name"} datatype={data.name} />
-					<TestdataComponent name={"Email"} datatype={data.email} />
-					<TestdataComponent name={"Password"} datatype={data.password} />
-					<TestdataComponent name={"Timezone"} datatype={data.timezone} />
-					<TestdataComponent name={"Phone"} datatype={data.phone} />
-					<TestdataComponent name={"Street"} datatype={data.street} />
-					<TestdataComponent name={"City"} datatype={data.city} />
-					<TestdataComponent name={"Country"} datatype={data.country} />
-					<TestdataComponent name={"State"} datatype={data.state} />
-					<TestdataComponent name={"Company"} datatype={data.company} />
-					<TestdataComponent name={"Department"} datatype={data.department} />
-					<TestdataComponent name={"Product"} datatype={data.product} />
-					<TestdataComponent name={"Vehicle"} datatype={data.vehicle} />
-					<TestdataComponent name={"UUID"} datatype={data.uuid} />
-					<TestdataComponent name={"Sentences"} datatype={data.sentences} />
-					<TestdataComponent name={"Paragraphs"} datatype={data.paragraphs} />
-					<TestdataComponent
-						isRandomWord={true}
-						name={"Random Words"}
-						datatype={data.words}
-						wordCount={wordCount}
-						handleWordCount={(e) => {
-							setWordCount(Number(e.target.value));
-						}}
-					/>
-					{/* 
-						<div className="col"></div>
-						<div className="col"></div> 
-					*/}
+					<div className="col">
+						<TestdataComponent name={"Name"} datatype={data.name} />
+						<TestdataComponent name={"Email"} datatype={data.email} />
+						<TestdataComponent name={"Password"} datatype={data.password} />
+						<TestdataComponent name={"Timezone"} datatype={data.timezone} />
+						<TestdataComponent name={"Phone"} datatype={data.phone} />
+						<TestdataComponent name={"Street"} datatype={data.street} />
+						<TestdataComponent name={"City"} datatype={data.city} />
+						<TestdataComponent name={"Country"} datatype={data.country} />
+						<TestdataComponent name={"State"} datatype={data.state} />
+						<TestdataComponent name={"Company"} datatype={data.company} />
+						<TestdataComponent name={"Department"} datatype={data.department} />
+						<TestdataComponent name={"Product"} datatype={data.product} />
+						<TestdataComponent name={"Vehicle"} datatype={data.vehicle} />
+						<TestdataComponent name={"File Name"} datatype={data.fileName} />
+						<TestdataComponent name={"File EXT"} datatype={data.fileExt} />
+						<TestdataComponent name={"File Type"} datatype={data.fileType} />
+						<TestdataComponent
+							isRandomWord={true}
+							name={"Random Words"}
+							datatype={data.words}
+							wordCount={wordCount}
+							handleWordCount={(e) => {
+								setWordCount(Number(e.target.value));
+							}}
+						/>
+					</div>
+					<div className="col">
+						<TestdataComponent name={"UUID"} datatype={data.uuid} />
+						<TestdataComponent name={"Job Title"} datatype={data.jobTitle} />
+						<TestdataComponent name={"Job Type"} datatype={data.jobType} />
+						<TestdataComponent
+							name={"Job Descriptor"}
+							datatype={data.jobDescriptor}
+						/>
+						<TestdataComponent name={"Prefix"} datatype={data.prefix} />
+						<TestdataComponent name={"Title"} datatype={data.title} />
+						<TestdataComponent name={"Genre"} datatype={data.genre} />
+						<TestdataComponent name={"Username"} datatype={data.userName} />
+						<TestdataComponent name={"URL"} datatype={data.url} />
+						<TestdataComponent
+							name={"Bitcoin Address"}
+							datatype={data.bitcoinAddress}
+						/>
+						<TestdataComponent
+							name={"Transaction Description"}
+							datatype={data.transactionDescription}
+						/>
+						<TestdataComponent
+							name={"Credit Card"}
+							datatype={data.creditCardNumber}
+						/>
+						<TestdataComponent name={"Color"} datatype={data.color} />
+						<TestdataComponent name={"Sentences"} datatype={data.sentences} />
+						<TestdataComponent name={"Paragraphs"} datatype={data.paragraphs} />
+					</div>
 				</div>
 			</div>
 		</>
