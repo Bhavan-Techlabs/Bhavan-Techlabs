00062a70f281b6f3345ede170fec47394519664a
bhavan <bhavanl@techprovint.com>
2022-04-18 23:54:06 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React from "react";
 import { HomePage, LoginPage } from "./components/pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
-    return (
-        <>
-            <HomePage />
-        </>
-    );
+function useToken() {
+    const getToken = () => {
+        const tokenString = sessionStorage.getItem("token");
+        const userToken = JSON.parse(tokenString);
+        return userToken?.token;
+    };
+
+    const [token, setToken] = React.useState(getToken());
+
+    const saveToken = (userToken) => {
+        sessionStorage.setItem("token", JSON.stringify(userToken));
+        setToken(userToken.token);
+    };
+
+    return {
+        setToken: saveToken,
+        token,
+    };
 }
 
-export default App;
+export default function App() {
+    const { token, setToken } = useToken();
+
+    if (!token) {
+        return <LoginPage setToken={setToken} />;
+    }
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
